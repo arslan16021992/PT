1ff0125d56d85b92eb92d9fce22f7948c3b5d276
Arslan Ubaydullaev <a.ubaydullaev@lcgroup.su>
2020-07-20 11:52:00 +0700

    fix tests

--- tests/test_shell_seq.py
+++ tests/test_shell_seq.py
@@ -15,31 +15,32 @@ class ShellCheck(unittest.TestCase):
         self.data_6 = [-1, -2, -3, -4, -5]
 
     def test_1(self):
-        self.assertRaises(TypeError, shell_seq.shell_sort(self.data_0))
+        with self.assertRaises(TypeError):
+            shell_seq.shell_sort(self.data_0)
 
     def test_2(self):
         shell_seq.shell_sort(self.data_1)
-        self.assertIsNotNone(self.data_1)
+        self.assertEqual(self.data_1, [])
 
     def test_3(self):
         shell_seq.shell_sort(self.data_2)
-        self.assertIsNotNone(self.data_2)
+        self.assertEqual(self.data_2, [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9])
 
     def test_4(self):
         shell_seq.shell_sort(self.data_3)
-        self.assertIsNotNone(self.data_3)
+        self.assertEqual(self.data_3, [1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9])
 
     def test_5(self):
         shell_seq.shell_sort(self.data_4)
-        self.assertIsNotNone(self.data_4)
+        self.assertEqual(self.data_4, ['Hello', 'world'])
 
     def test_6(self):
         shell_seq.shell_sort(self.data_5)
-        self.assertIsNotNone(self.data_5)
+        self.assertEqual(self.data_5, ['#', '$', '&', '@', '^'])
 
     def test_7(self):
         shell_seq.shell_sort(self.data_6)
-        self.assertIsNotNone(self.data_6)
+        self.assertEqual(self.data_6, [-5, -4, -3, -2, -1])
 
         # run = subprocess.Popen('python D:/RND/credit_robot-ru/tests/test_integration/shell_seq.py',
         #                        stdout=subprocess.PIPE, shell=True)
